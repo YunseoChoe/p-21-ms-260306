7778960ec17e673c9f347b9c394d7ea0df5fc627
choiyunseo <choeyunseo4060@naver.com>
2026-03-17 23:03:33 +0900

    Feat: 3-2

--- src/main/java/com/mysite/sbb/TestInitData.java
+++ src/main/java/com/mysite/sbb/TestInitData.java
@@ -3,6 +3,7 @@ package com.mysite.sbb;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -22,6 +23,7 @@ public class TestInitData {
     @Lazy
     private TestInitData self;
     private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @Bean
     ApplicationRunner initDataRunner() {
@@ -31,18 +33,11 @@ public class TestInitData {
                 return;
             }
 
-            System.out.println("데이터 등록.");
-            Question q1 = new Question();
-            q1.setSubject("sbb가 무엇인가요?");
-            q1.setContent("sbb에 대해서 알고 싶습니다.");
-            q1.setCreateDate(LocalDateTime.now());
-            this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-            Question q2 = new Question();
-            q2.setSubject("스프링부트 모델 질문입니다.");
-            q2.setContent("id는 자동으로 생성되나요?");
-            q2.setCreateDate(LocalDateTime.now());
-            this.questionRepository.save(q2);  // 두번째 질문 저장
+            for (int i = 1; i <= 300; i++) {
+                String subject = String.format("테스트 데이터입니다:[%03d]", i);
+                String content = "내용무";
+                this.questionService.create(subject, content);
+            }
         };
     }
 }
